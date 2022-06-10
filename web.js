dfe8c09843464e11fd67bd79e4c8772fd3d015f3
Sing <sing.chen@linecorp.com>
2022-06-10 12:53:01 +0800

    face to correct direction

--- web.js
+++ web.js
@@ -7,11 +7,84 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.send('Let the battle begin!');
 });
+const example = {
+  "_links": {
+    "self": {
+      "href": "https://foo.com"
+    }
+  },
+  "arena": {
+    "dims": [4, 3],
+    "state": {
+      "https://foo.com": {
+        "x": 0,
+        "y": 0,
+        "direction": "N",
+        "wasHit": false,
+        "score": 0
+      }
+    }
+  }
+}
+const DIRECTIONS = {
+  North: "N",
+  West: "W",
+  South: "S",
+  East: "E"
+}
 
+const MOVES = {
+  Forward: "F",
+  Throw: "T",
+  TurnLeft: "L",
+  TurnRight: "R"
+}
+
+const myUrl = 'https://hackathon-1120-wtpnck3waq-uc.a.run.app/'
 app.post('/', function (req, res) {
-  console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send('T');
+  console.log(JSON.stringify(req.body));
+  const myState = req.body.arena.state[myUrl]
+  const [arenaX, arenaY] = req.body.arena.dims
+  const othersState = Object.entries(req.body.arena.state).filter(([key]) => key !== myUrl).map(([key, val]) => {
+    console.log(key, val);
+    return { ...val, player: key }
+  })
+  const { x, y, direction, wasHit, score } = myState
+  // 1. Face to the right direction (Do not stuck)
+  // 2. Attack if there is someone in my direction
+  // 3. If no one close, move
+  // faceToCorrectDirection()
+  //function faceToCorrectDirection() {
+  if (y === 0 && direction === DIRECTIONS.North) {
+    if (x === 0) {
+      return res.send(MOVES.TurnRight)
+    }
+
+    if (x === areaX) {
+      return res.send(MOVES.TurnLeft)
+    }
+  }
+
+  if (y === arenaY && direction === DIRECTIONS.South) {
+    if (x === 0) {
+      return res.send(MOVES.TurnLeft)
+    }
+
+    if (x === areaX) {
+      return res.send(MOVES.TurnRight)
+    }
+  }
+
+  if (x === 0 && direction === DIRECTIONS.West) {
+    return res.send(MOVES.TurnLeft)
+  }
+
+  if (x === arenaX && direction === DIRECTIONS.East) {
+    return res.send(MOVES.TurnLeft)
+  }
+  // }
+
+  res.send(MOVES.Throw);
 });
 
 app.listen(process.env.PORT || 8080);
